6a398a466268adaa2cc7ecc33f772dea2ad50176
Kaile <yekaile@yahoo.com>
2017-02-14 23:39:55 -0500

    done this before

---
+++ LeetCode/SameTree/Solution.java
@@ -0,0 +1,16 @@
+package LeetCode.SameTree;
+
+// Given two binary trees, write a function to check if they are equal or not.
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
