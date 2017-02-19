ddf3c10160c14ff1070dcae2947ad49fe050c0b3
Kaile <yekaile@yahoo.com>
2017-02-19 01:30:30 -0500

    did this before

---
+++ LeetCode/FindTheDifference/Solution.java
@@ -0,0 +1,29 @@
+package LeetCode.FindTheDifference;
+
+/*
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char xor = 0;
+        for (char c : s.toCharArray()) {
+            xor ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            xor ^= c;
+        }
+        return xor;
+    }
+}
+*/
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char xor = 0;
+        for (char c : s.toCharArray()) {
+            xor ^= c;
+        }
+        for (char c : t.toCharArray()) {
+            xor ^= c;
+        }
+        return xor;
+    }
+}
