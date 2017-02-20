0365e80c708b1bdec5f2d7b993ed78206e5f14ce
Kaile <yekaile@yahoo.com>
2017-02-19 22:33:53 -0500

    anagram problem

---
+++ LeetCode/ValidAnagram/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode.ValidAnagram;
+
+/*
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+*/
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+}
